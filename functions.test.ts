5c4f50267cebc023b4a421823c1f9dd944c9330e
Kilroy-was-here1945 <84049904+Kilroy-was-here1945@users.noreply.github.com>
2022-07-04 13:03:02 -0600

    Update functions.test.ts

--- functions.test.ts
+++ functions.test.ts
@@ -1,32 +1,32 @@
-// const {shuffleArray} = require('./utils')
+const {shuffleArray} = require('./utils')
 
-// let testing1 = [1,2,3,4];
-// let testing2 = [];
-// let testing3 = [45,64,1,35,5];
+let testing1 = [1,2,3,4];
+let testing2 = [];
+let testing3 = [45,64,1,35,5];
 
-// const shuffled = shuffleArray([1, 2, 3, 4, 5, 6, 7])
+const shuffled = shuffleArray([1, 2, 3, 4, 5, 6, 7])
 
 
-// describe('shuffleArray should', () => {
-//     // CODE HERE
+describe('shuffleArray should', () => {
+    // CODE HERE
     
-//     test('testing the length stays the same', async () => {
-//         expect(shuffleArray(testing1).length).toBe(testing1.length)
-//         expect(shuffleArray(testing2).length).toBe(testing2.length)
-//         expect(shuffleArray(testing3).length).toBe(testing3.length)
-//     })
-//     test("doesn't change an array into something else", async () => {
-//         expect(Array.isArray(shuffleArray(testing1))).toBe(true)
-//         expect(Array.isArray(shuffleArray(testing2))).toBe(true)
-//         expect(Array.isArray(shuffleArray(testing3))).toBe(true)
-//     })
-//     // test('suffled has the same length', () => {
-//     //     expect(shuffled.length).toEqual(7)
-//     // })
+    test('testing the length stays the same', async () => {
+        expect(shuffleArray(testing1).length).toBe(testing1.length)
+        expect(shuffleArray(testing2).length).toBe(testing2.length)
+        expect(shuffleArray(testing3).length).toBe(testing3.length)
+    })
+    test("doesn't change an array into something else", async () => {
+        expect(Array.isArray(shuffleArray(testing1))).toBe(true)
+        expect(Array.isArray(shuffleArray(testing2))).toBe(true)
+        expect(Array.isArray(shuffleArray(testing3))).toBe(true)
+    })
+    test('suffled has the same length', () => {
+        expect(shuffled.length).toEqual(7)
+    })
     
     
-//     // test("testing how long the array is", () => {
-//     //     expect(Array.isArray(shuffled)).toEqual(true)
-//     //     // console.log(shuffleArray.length)
-//     // })
-// })
+    test("testing how long the array is", () => {
+        expect(Array.isArray(shuffled)).toEqual(true)
+        // console.log(shuffleArray.length)
+    })
+})
