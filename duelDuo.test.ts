0ea7c79f6aa1a4c3466f53f65751152cf70ea46c
Kilroy-was-here1945 <84049904+Kilroy-was-here1945@users.noreply.github.com>
2022-07-05 18:11:50 -0600

    Update duelDuo.test.ts

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -12,28 +12,16 @@ beforeEach(async () => {
 afterAll(async () => {
     driver.quit()
 })
-
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('choices'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-}
-)
+test("Make sure clicking draw shows choices", async () => {
+    await driver.findElement(By.xpath(`//button[@id="draw"]`)).click();
+    expect(await (driver.findElement(By.xpath(`//div[@id="choices"]`))).isDisplayed()).toBe(true);
+    await driver.sleep(1000);
+  });
 
 
+test('Title shows up on load', async () => {expect(await driver.findElement(By.id("title")).isDisplayed()).toBe(true);})
 
 
-test('object assigment', () => {
-    const data = {one: 1};
-    data['two'] = 2;
-    expect(data).toEqual({one: 1, two:2});
-});
 
 
-test('adding positive numbers is not zero', () => {
-    for (let a = 1; a < 10; a++) {
-        for (let b = 1; b < 10; b++) {
-            expect(a + b).not.toBe(0);
-        }
-    }
-});
+
